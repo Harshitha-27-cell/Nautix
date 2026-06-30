2038d63d8ce88c703a1576b990fe0236e0c01196
Harshitha <152185823+KachapuramHarshitha123@users.noreply.github.com>
2026-06-30 10:58:21 +0530

    Fix avatar_url migration

--- Backend/migrations/versions/73f2659289dc_add_avatar_url_column.py
+++ Backend/migrations/versions/73f2659289dc_add_avatar_url_column.py
@@ -3,24 +3,25 @@
 Revision ID: 73f2659289dc
 Revises: 28f5da76e986
 Create Date: 2026-06-30 10:47:32.060366
-
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-
-# revision identifiers, used by Alembic.
-revision: str = '73f2659289dc'
-down_revision: Union[str, None] = '28f5da76e986'
+revision: str = "73f2659289dc"
+down_revision: Union[str, None] = "28f5da76e986"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    pass
+    op.add_column(
+        "users",
+        sa.Column("avatar_url", sa.String(length=512), nullable=True),
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_column("users", "avatar_url")
